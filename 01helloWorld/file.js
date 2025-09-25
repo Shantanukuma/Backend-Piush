813cb73b0af44f698690adadae48a90247e15499
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-09-25 10:58:42 +0530

    File system

---
+++ 01helloWorld/file.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// fs.writeFileSync('./test.txt', 'hey there', )
+// fs.writeFile('./test.txt', 'hello world', (error) => {})
+
+
+
+module.exports = {
+    fs,
+}
--- 01helloWorld/hello.js
+++ 01helloWorld/hello.js
@@ -1,5 +1,17 @@
 const {add, sub, fix} = require('./math');
-console.log(add(5, 7))
-console.log(sub(5, 7))
-console.log("Fix is", fix);
+const {fs} = require('./file')
+// console.log(add(5, 7))
+// console.log(sub(5, 7))
+// console.log("Fix is", fix);
+fs.appendFileSync('./test.txt', "ABCCC",)
+console.log('File updated successfully');
+console.log(fs.readFileSync('./test.txt', 'utf-8', ))
+fs.appendFileSync('./test.txt', "Mab\n", )
+console.log(fs.readFileSync('./test.txt', 'utf-8', ))
+// fs.cpSync('./text.txt', './copy.txt')
+fs.unlinkSync('./copy.txt')
+
+
+
+
 
