bae778058451547b83a210f772239b0e666698d9
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-09-25 09:46:27 +0530

    module, export

---
+++ 01helloWorld/hello.js
@@ -0,0 +1,5 @@
+const {add, sub, fix} = require('./math');
+console.log(add(5, 7))
+console.log(sub(5, 7))
+console.log("Fix is", fix);
+
---
+++ 01helloWorld/math.js
@@ -0,0 +1,15 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+const fix = 9
+
+module.exports = {
+    add,
+    sub,
+    fix
+}
+
