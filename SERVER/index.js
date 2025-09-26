7a522ac04854f000179b6985c0dffb7ae69b64a7
Shantanu Kumar Nath <shantanukumarnath55@gmail.com>
2025-09-26 07:49:01 +0530

    Express

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,15 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const http = require('http')
 
-const myServer = http.createServer( (req, res) => {
-    const log = `${Date.now()}: New Req Received\n`
-    fs.appendFile('res.txt', log, (err, data) => {
-        res.end("Hello from server again. ")
-    })
-});
 
-myServer.listen(59, () => {
-    console.log("Server started");
-    
+const app = express()
+
+app.get('/',(req, res) => {
+    res.send("Home Page")
 })
+app.get('/about', (req, res) => {
+    res.send("About Page.")
+})
+
+app.listen(4500, () => {
+    console.log("Server started in port 4500.");
+})
+
+
+
+// const myServer = http.createServer(app)
+// myServer.listen(5000, () => {
+//     console.log("Server started.");
+// })
+
+
 
